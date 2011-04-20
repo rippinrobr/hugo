d23b1f393c264220300ffd1a9dc8ff2882c2b608
rippinrobr <rippinrobr@hotmail.com>
2011-04-20 16:26:13 -0400

    Have a version that will parse out the 2010 Hugo Award winners and nominees for the Best Novel category

--- src/cmdline.clj
+++ src/cmdline.clj
@@ -1,6 +1,10 @@
 (ns cmdline
   (:gen-class)
-  (:use hugo.parser))
+  (:use hugo.parser)
+  (:require [clojure.contrib.duck-streams :as duck]))
+
+(def url "http://www.thehugoawards.org/hugo-history/2010-hugo-awards/")
 
 (defn -main [& args]
-  (println (map #(format "%s\n" (:award %)) (hugo.parser/get-awards-per-year "http://www.thehugoawards.org/hugo-history/2010-hugo-awards/"))))
+  (let [novels (first (hugo.parser/get-awards-per-year url))]
+    (duck/spit "2010_hugo_awards.txt" (format "%s%s\n" (:award novels) (hugo.parser/format-nominees (:books novels))))))
--- src/hugo/parser.clj
+++ src/hugo/parser.clj
@@ -1,31 +1,26 @@
 (ns hugo.parser
-(:require [net.cgrand.enlive-html :as html]))
+ (:require [net.cgrand.enlive-html :as html])
+ (:require [clojure.contrib.duck-streams :as duck])
+ (:require [clojure.contrib.string :as ccstring]))
 
 (def *base-url* "http://www.thehugoawards.org/hugo-history/")
 
 (defn fetch-url [url] (html/html-resource (java.net.URL. url)))
 
-(defn get-award-links []
-  (map #(:attrs %)   (html/select (fetch-url *base-url*)
-                    #{[:div#content :li.page_item :a] [:li.page.item.subtext html/first-child]})))
-
 (defn get-book-info [nominees]
   (map #(if (not (nil? (first (:content (first (:content %)))))) (merge {:winner (if (not (nil? (:attrs %))) (:class (:attrs %)))} 
-               {:title (str (first (:content (first (:content %)))) (second (:content %)))}
+               {:title (str (ccstring/replace-str "\"" "" (first (:content (first (:content %))))) (second (:content %)))}
                )) nominees))
 
 (defn parse-award-page [url]
   (partition 2 (interleave 
                          (last (split-at 3 (keep #(if (nil? (:attrs %)) (:content %)) 
                                               (html/select (fetch-url url) #{[:div#content (html/but :p.tropy) :p] [:p html/first-child]}))))
-  			 (map #(:content %) (html/select (fetch-url url) #{[:div#content :ul] }))) ))
+  			 (map #(:content %) (html/select (fetch-url url) #{[:div#content :ul ] }))) ))
 
 (defn get-awards-per-year [url]
   (map #(merge {:award (first (first %))} 
                {:books (get-book-info (rest (second %)))}) (parse-award-page url)))
-
-(defn get-awards []
-  (map #(merge % {:nominees (get-awards-per-year (:href %))}) (get-award-links)))
 
 (defn if-work-not-nil [work]
   (str (if (not (nil? (:title work))) (str "\n\t" (:title work)) ) (if (not (nil? (:winner work))) (str " (WINNER)") ) ""))
@@ -33,8 +28,18 @@
 (defn format-nominees [works]
   (apply str (map if-work-not-nil works)))
 
-(defn format-output [awards]
-  (map #(str "\n" (:award %) (format-nominees (:books %)) "\n") awards))
+;--------------------------------------------------------------------------
+; The code under he will come into play in the second post on this subject
+;(defn get-award-links []
+  ;(map #(:attrs %)   (html/select (fetch-url *base-url*)
+                    ;#{[:div#content :li.page_item :a] [:li.page.item.subtext html/first-child]})))
 
-(defn testaa [rec]
-  (map #(str (apply str (:content (first (:content %)))) " " (apply str (second (:content %)))) rec))
+;(defn get-awards []
+;  (map #(merge % {:nominees (first (get-awards-per-year (:href %)))}) (get-award-links)))
+
+;(defn format-output [awards]
+  ;(map #(str "\n" (:award %) (format-nominees (:books %)) "\n") awards))
+
+;(defn testaa [rec]
+  ;(duck/spit "2010_hugo.txt" (apply str (format-output rec))))
+
