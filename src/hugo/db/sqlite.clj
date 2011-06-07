3c3602a2a5fa43813cc3bea4cb164d57323daccf
rippinrobr <rippinrobr@hotmail.com>
2011-06-06 21:49:23 -0400

    removed unneeded sql related functions

--- src/hugo/db/sqlite.clj
+++ src/hugo/db/sqlite.clj
@@ -17,23 +17,9 @@
     (sql/with-query-results results sql-stmt
       (doall results))))
 
-(defn add-nominee
-  [year nominee]
-   (with-connection db (sql/insert-values :nominees 
-     [:year :title :author :winner] 
-          [year (:title nominee) (:author nominee) 
-           (if (nil? (:winner nominee)) 0 1)])))
-
-(defn get-nominee
-  ([id] (get-sql [(str get-all-nominees " where id=?") id])))
-
 (defn get-nominees
   ([] (get-sql [get-all-nominees]))
   ([year] (get-sql [(str get-all-nominees " where year=?") year])))
-
-(defn get-winners
-  ([] (get-sql [(str get-all-nominees " where winner=1")]))
-  ([year] (get-sql [(str get-all-nominees " where winner=1 and year=?") year])))
 
 (defn get-years []
   (get-sql ["select distinct year from nominees order by year desc"]))
